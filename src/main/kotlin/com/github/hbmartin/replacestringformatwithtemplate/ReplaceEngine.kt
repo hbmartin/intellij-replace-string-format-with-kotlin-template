26917a6f8982ce53b43332bb9c5d918d3231f4c2
Harold Martin <harold.martin@gmail.com>
2022-04-19 18:30:36 -0700

    improved replacement formatting

--- src/main/kotlin/com/github/hbmartin/replacestringformatwithtemplate/ReplaceEngine.kt
+++ src/main/kotlin/com/github/hbmartin/replacestringformatwithtemplate/ReplaceEngine.kt
@@ -4,28 +4,30 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.executeCommand
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.idea.formatter.commitAndUnblockDocument
+import org.jetbrains.kotlin.idea.inspections.RemoveCurlyBracesFromTemplateInspection
+import org.jetbrains.kotlin.idea.inspections.findExistingEditor
+import org.jetbrains.kotlin.psi.KtBlockStringTemplateEntry
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.psi.KtValueArgumentList
 import org.jetbrains.kotlin.psi.psiUtil.getChildOfType
+import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
 
 object ReplaceEngine {
-    private val splitAt = "%\\w".toRegex()
+    private val splitAt = "(?<!%)%\\w".toRegex()
+    private val removeCurlyBracesFromTemplateInspection = RemoveCurlyBracesFromTemplateInspection()
 
     internal fun replaceFormatWithTemplate(dotQualExpr: KtDotQualifiedExpression) {
         val callExpr = dotQualExpr.getChildOfType<KtCallExpression>()
         callExpr?.getChildOfType<KtValueArgumentList>()?.let { args ->
-            println(args.arguments.first().node.text)
             val splitFormatting = args.arguments.first().node.text.split(splitAt)
-            println(splitFormatting)
             val templatedStringParts = splitFormatting.mapIndexed { index, el ->
                 args.arguments.getOrNull(index + 1)?.let { arg ->
                     "$el\${${arg.node.text}}"
                 } ?: el
             }
             val templatedString = templatedStringParts.joinToString(separator = "").replace("%%", "%")
-            println(templatedString)
             dotQualExpr.replaceWithTemplateString(templatedString)
         }
     }
@@ -36,9 +38,12 @@ object ReplaceEngine {
         app.invokeLater {
             executeCommand(project = project) {
                 app.runWriteAction {
-                    this.replace(
-                        KtPsiFactory(this.project).createExpression(template)
-                    )
+                    val expression = KtPsiFactory(this.project).createExpression(template)
+                    expression.getChildrenOfType<KtBlockStringTemplateEntry>().forEach {
+                        removeCurlyBracesFromTemplateInspection.applyTo(it, project, this.findExistingEditor())
+                    }
+
+                    this.replace(expression)
                     this.containingFile.commitAndUnblockDocument()
                 }
             }
